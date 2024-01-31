72c03cf77a0789d7ee52e6218a451fcdc2fa3d0d
Himanshu Gupta <ghimax23@gmail.com>
2024-01-31 13:43:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/twitter", (req, res) => {
+  res.send("https://twitter.com/ayy_him_anshu23");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>hello</h1>");
+});
+app.get("/instagram", (req, res) => {
+  res.send("<h1>him.anshu_.23</h1>");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
